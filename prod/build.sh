40eed0ca0bd023e66bd137e6d929a1cee9ba800e
Selvedin Dokara <selvax00y@hotmail.com>
2020-04-26 13:46:21 +0200

    idevcorp image tag

--- prod/build.sh
+++ prod/build.sh
@@ -3,4 +3,4 @@
 if [ "$(basename "$(pwd)")" == "prod" ]; then echo "Run me from project root as ./prod/$(basename "$0")" && exit 1; fi
 
 make build
-docker build -t asherda/lightwalletd:latest -f ./prod/Dockerfile .
+docker build -t infra.valu.idevcorp.net:5000/idevcorp/lightwalletd:latest -f ./prod/Dockerfile .
--- prod/start.sh
+++ prod/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --name lightwalletd --rm --env-file ./prod/.env -p 9067:9067 asherda/lightwalletd:latest
+docker run -it --name lightwalletd --rm --env-file ./prod/.env -p 9067:9067 infra.valu.idevcorp.net:5000/idevcorp/lightwalletd:latest
